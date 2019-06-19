43b497d982da6753dc52003d2fc2f9d074a8ab7f
Masato Oshima <qqw75pn9@gmail.com>
2019-06-19 13:32:46 +0900

    Change tab

--- provider.go
+++ provider.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-        "github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
+// This is the root of the provider
 func Provider() *schema.Provider {
-        return &schema.Provider{
-                ResourcesMap: map[string]*schema.Resource{},
-        }
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
 }
