a3e253ac3eb8bd7bcf804e72765f2d8371d44e53
Masato Ohshima <m_ohshima@r.recruit.co.jp>
2019-06-28 18:28:54 +0900

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,21 +1,33 @@
 provider "twilio" {
-    # account_sid = "" // You can set env variables TWILIO_ACCOUNT_SID
-    # auth_token = ""  // You can set env variables TWILIO_AUTH_TOKEN
+  # account_sid = "" // You can set env variables TWILIO_ACCOUNT_SID  # auth_token = ""  // You can set env variables TWILIO_AUTH_TOKEN
 }
 
-
 resource "twilio_chat_service" "test" {
-    friendly_name = "test21"
-    limits {
-        channel_members = 1
-        user_channels = 2
-    }
+  friendly_name = "test21"
+
+  limits {
+    channel_members = 1
+    user_channels   = 2
+  }
 }
 
 resource "twilio_chat_service" "test2" {
-    friendly_name = "test23"
-    limits {
-        channel_members = 3
-        user_channels = 4
-    }
+  friendly_name    = "test23"
+  pre_webhook_url  = "https://httpbin.org/get"
+  post_webhook_url = "https://httpbin.org/post"
+  webhook_method   = "POST"
+
+  webhook_filters = [
+    "onMessageSend",
+    "onMessageSent",
+    "onChannelUpdated",
+    "onMessageUpdated",
+    "onMessageUpdate",
+    "onUserUpdate",
+  ]
+
+  limits {
+    channel_members = 3
+    user_channels   = 4
+  }
 }
